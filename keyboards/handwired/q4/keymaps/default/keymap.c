f9c94c622415c1004a467ebffd2b67bf2bb624bd
Quinlan Pfiffer <qpfiffer@gmail.com>
2025-12-14 21:20:27 -0700

    Lets see if this works

--- keyboards/handwired/q4/keymaps/default/keymap.c
+++ keyboards/handwired/q4/keymaps/default/keymap.c
@@ -2,19 +2,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
-#include <qp.h>
-
-static painter_device_t       lcd;
-
-void keyboard_post_init_user(void) {
-    // Init the display
-    ui_init();
-}
-
-void housekeeping_task_user(void) {
-    // Draw the display
-    ui_task();
-}
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
@@ -29,6 +16,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───┴───┴───┴───┘
      */
     [0] = LAYOUT(
-        KC_P4, KC_P7, KC_P8, KC_P9
+        KC_F1, KC_F2, KC_F3, KC_F4
     )
 };
